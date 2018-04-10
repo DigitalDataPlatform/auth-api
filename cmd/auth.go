6a5a4f51ac27b9ca16cda87c27fd7b78783dd32c
Fabrice CLAEYS <fabrice.claeys@symbol-it.fr>
2018-04-10 17:00:31 +0200

    feat(login): send JWT token on login

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,25 +1,63 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/apex/log"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/gobuffalo/envy"
 )
 
+var hmacSampleSecret []byte
+
 type AuthData struct {
-	AccessToken string `json="accessToken"`
-	IDToken     string `json="id_token"`
+	AccessToken string `json:"accessToken"`
+	IDToken     string `json:"id_token"`
 }
 
 type UserInfo struct {
 	Login string `json="login"`
 }
 
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		w.WriteHeader(http.StatusPreconditionFailed)
+		return
+	}
+
+	password := r.URL.Query().Get("password")
+	if password == "" {
+		w.WriteHeader(http.StatusPreconditionFailed)
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iss":      "digital-data-platform",
+		"iat":      time.Now().Unix(),
+		"exp":      time.Now().Add(30 * time.Minute).Unix(),
+		"sub":      "digital-data-platform",
+		"aud":      "digital-data-platform",
+		"user":     "20009473",
+		"username": "fclaeys",
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte("monsupersecretvraimentsupersecret"))
+	if err != nil {
+		log.WithError(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(tokenString))
+}
+
 func main() {
+
 	port := ":" + envy.Get("PORT", "3000")
 
 	r := chi.NewRouter()
@@ -28,17 +66,7 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/oauth/token", func(w http.ResponseWriter, r *http.Request) {
-		fakeToken := AuthData{AccessToken: "xxxxxxxyyyyyyyyyzzzz", IDToken: "rrrrrrrrrrrrrrrtttttttttttttttt"}
-		b, _ := json.Marshal(fakeToken)
-		w.Write(b)
-	})
-
-	r.Get("/userinfo", func(w http.ResponseWriter, r *http.Request) {
-		fakeUser := UserInfo{Login: "20009473"}
-		b, _ := json.Marshal(fakeUser)
-		w.Write(b)
-	})
+	r.Get("/login", LoginHandler)
 
 	log.Infof("======== App running in %v mode ========", "stage")
 	http.ListenAndServe(port, r)
